bdbaf990b6b2f8d36d12a22d9223f54fab8391ab
Babalwa01 <babalwambolekwa93@gmail.com>
2021-03-06 08:49:19 +0200

    tests modified

--- spec/score_spec.js
+++ spec/score_spec.js
@@ -1,25 +1,31 @@
+const {outputGrade} = require('../src/score');
+
 describe('Grade output function', () => {
     it('should return a grade of A', () => {
-        expect(outputGrade(0.9).toBe('A'));
+        expect(outputGrade(0.9)).toBe('A');
     });
 
     it('should return a grade of B', () => {
-        expect(outputGrade(0.8).toBe('B'));
+        expect(outputGrade(0.8)).toBe('B');
     });
 
     it('should return a grade of C', () => {
-        expect(outputGrade(0.7).toBe('C'));
+        expect(outputGrade(0.7)).toBe('C');
     });
 
     it('should return a grade of D', () => {
-        expect(outputGrade(0.6).toBe('D'));
+        expect(outputGrade(0.6)).toBe('D');
     });
 
     it('should return a grade of F', () => {
-        expect(outputGrade(0.5).toBe('F'));
+        expect(outputGrade(0.5)).toBe('F');
     });
 
     it('should throw an error if the number is out of range', () => {
-        expect(function() {outputGrade(-1)}).toThrow('input must not be null / must be a number');
+        expect(outputGrade(-1)).toBe('input must not be null / must be a number');
+    });
+
+    it('should throw an error if the number is out of range', () => {
+        expect(outputGrade(1.3)).toBe('input must not be null / must be a number');
     });
 });
